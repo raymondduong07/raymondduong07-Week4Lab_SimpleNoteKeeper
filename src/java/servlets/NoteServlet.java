bd7106dc2622abd55c9bb82265e3dcdfa9f90038
rayfi <rayfi@DESKTOP-EUP6709.hitronhub.home>
2020-10-03 13:08:21 -0600

    Populated the Note Java Bean

--- src/java/servlets/NoteServlet.java
+++ src/java/servlets/NoteServlet.java
@@ -1,6 +1,6 @@
 package servlets;
 
-import java.io.IOException;
+import java.io.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +15,14 @@ public class NoteServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+        
+        // to read files
+        BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+        String title = br.readLine();
+        String contents = "";
+        
+        
         getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp")
                 .forward(request, response);
     } 
@@ -22,6 +30,10 @@ public class NoteServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+        // to write to a file
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path, false))); 
+        
         getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp")
                 .forward(request, response);
     }
